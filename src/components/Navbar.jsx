e1ddbad5665be96462eda6f37933e9b1ce5b6a73
josephsbucholtz <joseph.bucholtz@sjsu.edu>
2025-08-02 18:10:00 -0700

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Navbar = () => {
   return (
-    <div className="fixed inset-x-0 top-4 z-50 h-10 border-b-4 border-black transition-all duration-700 sm:inset-x-6">
+    <div className="fixed inset-x-0 top-4 z-50 h-10 border-b-4 border-black transition-all duration-700 sm:inset-x-6 bg-primary-theme bg-opacity-80 rounded-md">
       <header className="absolute top-1/2 w-full -translate-y-1/2">
         <nav className="flex size-full items-center justify-between p-4">
           {/* Left: Logo */}
@@ -32,7 +32,7 @@ export const Navbar = () => {
                 <a href="#works" className="nav-hover-btn">
                     works
                 </a>
-                <div className="absolute right-1 mt-2 w-80 rounded-xl bg-primary-theme border border-black shadow-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-50 pointer-events-auto">
+                <div className="absolute mt-2 w-80 rounded-xl bg-primary-theme border border-black shadow-xl opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 z-50 pointer-events-auto">
                     <ul className="py-2 text-base text-[#080808] font-tinos tracking-wide">
                     <li>
                         <a href="#Project_fan_bridge" className="block px-6 py-3 transition-all duration-300 hover:bg-[#f6f6f6] hover:pl-8 hover:shadow-inner">
