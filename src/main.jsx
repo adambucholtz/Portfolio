1aecbea0bd6f65d04d438a17b93af1a107805e65
adambucholtz <ahbucholtz@cpp.edu>
2025-08-09 16:20:25 -0700

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter as Router } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import 'flowbite'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
